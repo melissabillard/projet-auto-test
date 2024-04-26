b593717bd1a858428d29d817e58ad4f7a49a5a97
unknown <billardmelissa1@gmail.com>
2024-04-26 15:42:06 +0200

    bouton vers interface 2

--- src/markup/pages/home/index.jsx
+++ src/markup/pages/home/index.jsx
@@ -16,15 +16,12 @@ export default function Index() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" style={styles.logo} />
                 <p>
-                    Bonjour 👋
+                    Bonjour ! 👋
                 </p>
-                <a
-                    className="App-link"
-                    href="/page-1"
-                    rel="noopener noreferrer"
-                >
-                    **TOCTOC**
+                <a class="pikachu-button" href="/page-1" rel="noopener noreferrer">
+                    Attrape ton pokémon 
                 </a>
+
             </header>
         </>
     )
