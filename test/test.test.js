0b3199909be4658f691e014ef99a9b58cb4389fc
melissabillard <billardmelissa1@gmail.com>
2024-05-15 16:38:27 +0200

    add premier test

--- test/test.test.js
+++ test/test.test.js
@@ -1,16 +1,15 @@
-const unitaire = require('./unitaire');
+const sum = require('./unitaire');
 
 /* *
  * Execution test unitaire
  * */ 
 test('adds 1 + 2 to equal 3', () => {
-  expect(unitaire(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 });
 
 /* *
  * Execution test graphique
  * */ 
-
 
 /* *
  * Execution test intégration
