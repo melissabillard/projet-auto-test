49b8da412780aa46d01605e02c0f08ec3a734a4e
melissabillard <billardmelissa1@gmail.com>
2024-06-06 15:31:31 +0200

    test intégration 2

--- __test__/integration/navigation.test.js
+++ __test__/integration/navigation.test.js
@@ -3,6 +3,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import Home from '../../src/markup/pages/home/index';
 import Page2 from '../../src/markup/pages/page-2/index';
+import Page1 from '../../src/markup/pages/page-1';
 import '@testing-library/jest-dom/extend-expect';
 
 
@@ -29,5 +30,24 @@ describe('Home to Page2 Integration Test', () => {
     expect(await screen.findByText('Pokémon Battle Simulator')).toBeInTheDocument();
 
     expect(screen.queryByText('Bonjour ! 👋')).not.toBeInTheDocument();
+  });
+});
+
+describe('Page1 to Home Integration Test', () => {
+  test('navigates from Page1 to Home', async () => {
+    render(
+      <MemoryRouter initialEntries={['/page-1']}>
+        <Routes>
+          <Route path="/page-1" element={<Page1 />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    // Verify Page1 initial content
+    expect(screen.getByText('Retour en arrière')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Retour en arrière'));
+    expect(await screen.findByText('Bonjour ! 👋')).toBeInTheDocument();
   });
 });
